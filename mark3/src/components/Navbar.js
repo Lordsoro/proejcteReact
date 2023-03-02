f218c70c86f7aad365eee092e446ec6257ebf7c0
Lordsoro <98828393+Lordsoro@users.noreply.github.com>
2023-03-02 18:28:09 +0100

    Update Navbar.js

--- mark3/src/components/Navbar.js
+++ mark3/src/components/Navbar.js
@@ -20,30 +20,41 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="navBar">
-            <div className="menuToggle" onClick={toggleMenu}>
-                <div className="hamburger"></div>
-            </div>
-            <ul className={`navLinks ${showMenu ? 'show' : ''}`}>
-                <li id='logo'><Link to="/">Home</Link></li>
-                <li><Link to='/invierno'>Outfits Invierno</Link></li>
-                <li><Link to="/verano">Outfits Verano</Link></li>
-                <li><Link to="/primavera">Outfits EntreTiempo</Link></li>
-                <li><a href="/accesorios">Accesorios</a></li>
-                {user ? (
-                    <>
-                        <li className='userloged' onClick={Logout}><Link to="/">Logout</Link></li>
-                        <li className='userloged'><Link to="/cart">🛒<span>{cartLength}</span></Link></li>
-                        <li className='userloged'>¡Bienvenido, {user.name}!</li>
-                    </>
-                ) : (
-                    <>
-                        <li className='userloged'><Link to="/login">Login</Link></li>
-                        <li className='userloged'><Link to="/register">Sign in</Link></li>
-                        <li className='userloged'>Registrate ➪</li>
-                    </>
-                )}
-            </ul>
-        </nav>
+        <div >
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <div className="navbar-nav">
+                            <li id='logo'><Link to="/">Home</Link></li>
+                            <li><Link to='/invierno'>Outfits Invierno</Link></li>
+                            <li><Link to="/verano">Outfits Verano</Link></li>
+                            <li><Link to="/primavera">Outfits EntreTiempo</Link></li>
+                            <li><a href="/accesorios">Accesorios</a></li>
+
+                        </div>
+                        <div className="navbar-nav ms-auto">
+                            {user ? (
+                                <>
+                                    <li className='userloged'>¡Bienvenido, {user}!</li>
+                                    <li className='userloged'><Link to="/cart">🛒<span>{cartLength}</span></Link></li>
+                                    <li className='userloged' onClick={Logout}><Link to="/">Logout</Link></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className='userloged'>Registrate ➪</li>
+                                    <li className='userloged'><Link to="/login">Login</Link></li>
+                                    <li className='userloged'><Link to="/register">Sign in</Link></li>
+
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
     );
 }
